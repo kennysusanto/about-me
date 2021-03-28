3abae6b0f490981d4fe554fb25665a8880252d09
kennysusanto <kenny.susanto@rocketmail.com>
2021-03-28 21:16:16 +0700

    2nd commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div class="home">
+            <h1>Kenny Susanto</h1>
+            <h3>Online Portfolio</h3>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Menu() {
+    return (
+        <div>
+            <ul>
+                <li>ICON</li>
+                <li>Certifications</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Menu;
