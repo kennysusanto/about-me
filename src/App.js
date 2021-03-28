ec1f4cd0854c94e17ed87cdb64964d4b4a27cc8d
kennysusanto <kenny.susanto@rocketmail.com>
2021-03-29 01:28:09 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
-import Menu from './Menu';
+import Title from './Title';
 
 function App() {
   return (
-    <div className="App">
-      <Menu />
-      <Home />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Powered by react.js</code>
-        </p>
-      </header>
+    
+    <div className="app">
+      <div className="menu">
+        <ul>
+          <li class="menu-left">ICON</li>
+          <li class="menu-right">Contact</li>
+          <li class="menu-right">Certifications</li>
+          
+        </ul>
+      </div>
       
+      <div className="content">
+        <div className="title">
+          <Title />
+          <h1>Kenny Susanto</h1>
+          <p>
+            <code>Powered by react.js</code>
+          </p>
+        </div>
+        
+        <div className="logo">
+          <div className="ripple"></div>
+          <img src={logo} className="content-logo" alt="logo" />
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="title">
+            
+        </div>
+    );
+}
+
+export default Title;
